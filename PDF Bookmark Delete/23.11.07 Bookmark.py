da9a13b792cc1a2083709565eb5f2a647babe2a9
lulmaoatwork <lul@gmail.com>
2024-01-29 10:29:33 +0700

    storage on GitHub

---
+++ PDF Bookmark Delete/23.11.07 Bookmark.py
@@ -0,0 +1,43 @@
+import fitz
+import re
+import sys
+import time
+from plyer import notification
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename()
+print(file_path)  # input document filename
+
+if __name__ == "__main__":
+    try:
+
+        doc = fitz.open(file_path)
+        toc = doc.get_toc(simple=False)
+        toc_new = [a for a in toc if a[0]==1 or (a[0] <= 3 and re.search(r'^((\d+)(\.)){1,2}\d+([^\.\d]|$)',a[1]))]
+        doc.set_toc(toc_new)
+        doc.saveIncr()
+
+        while True:
+            time.sleep(1)
+            notification.notify(
+                title = "Finished executing "+sys.argv[0],
+                message = "Successful",
+            )
+            #print('Successful')
+            #time.sleep(1)
+            break
+
+    except Exception as e:
+        while True:
+            time.sleep(1)
+            notification.notify(
+                title = "Error in executing "+sys.argv[0],
+                message = str(e),
+            )
+            #print(e)
+            #time.sleep(1)
+            break
